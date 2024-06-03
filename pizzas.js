259a6c13d23ddaf4f60cc6b4b54244b4103f1cc2
StormX90 <160551155+StormX90@users.noreply.github.com>
2024-06-03 20:48:33 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'fantasia',
+        img: 'images/c1.png',
+        price: [25.00, 25.00, 25.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'feminina'
+      },
+      {
+        id: 2,
+        name: 'come come',
+        img: 'images/c2.png',
+        price: [26.00, 26.00, 26.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'unisex'
+      },
+      {
+        id: 3,
+        name: 'unicórnio',
+        img: 'images/c3.png',
+        price: [28.00, 28.00, 28.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 anos'
+        ],
+        description: 'feminina'
+      },
+      {
+        id: 4,
+        name: 'Sereia',
+        img: 'images/c4.png',
+        price: [30.00, 30.00, 30.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'feminina'
+      },
+      {
+        id: 5,
+        name: 'urso poh',
+        img: 'images/c5.png',
+        price: [29.00, 29.00, 29.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'unisex'
+      },
+      {
+        id: 6,
+        name: 'kanguru',
+        img: 'images/c6.png',
+        price: [35.00, 35.00, 35.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'unisex'
+      },
+      {
+        id: 7,
+        name: 'Banguela',
+        img: 'images/c7.png',
+        price: [32.00, 32.00, 32.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'unisex'
+      },
+      {
+        id: 8,
+        name: 'baby body',
+        img: 'images/c8.png',
+        price: [35.00, 35.00, 35.00],
+        sizes: [
+          'até 24 Kg',
+          'até 38 Kg',
+          'até 49 Kg'
+        ],
+        description: 'feminina'
+      }
+];
